c7370f8c0bdaf0786795169ee26d2e2d378757fc
grahamwwilson <gwwilson@ku.edu>
2023-03-19 18:09:56 -0500

    add comments

--- threebody.sh
+++ threebody.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Compile the code and link with ROOT 
 ./clroot.sh
+
+# Read each line of TChiWZ.dat and calculate corresponding normalization constants
+# using ThreeBodyIntegral executable
+# Output file for reweighting is TChiWZgridWeights.dat
 
 while read -r a b; do
     echo "$a" "$b"
